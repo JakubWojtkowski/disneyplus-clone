915862a941f835e6fbf9944829be5cd8891a9ce3
wojtko <j.w2001@onet.pl>
2023-08-07 19:45:30 +0200

    Create header. Add styles. Add favicon.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,34 @@ import styled from "styled-components";
 function Header() {
   return (
     <Nav>
-      <Logo src=""></Logo>
+      <Logo src="/images/logo.svg"></Logo>
       <NavMenu>
-
+        <a>
+          <img src="/images/home-icon.svg" alt="icon"></img>
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" alt="icon"></img>
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" alt="icon"></img>
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" alt="icon"></img>
+          <span>ORIGINALS</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" alt="icon"></img>
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" alt="icon"></img>
+          <span>SERIES</span>
+        </a>
       </NavMenu>
+      <UserImg src="/images/elon.jpg"></UserImg>
     </Nav>
   );
 }
@@ -17,6 +41,9 @@ export default Header;
 const Nav = styled.nav`
   height: 70px;
   background-color: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
 `;
 
 const Logo = styled.img`
@@ -24,4 +51,31 @@ const Logo = styled.img`
 `;
 
 const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.4px;
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
 `;
