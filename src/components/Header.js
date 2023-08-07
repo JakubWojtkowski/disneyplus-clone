9d462ececb7f1ee7b617fe81f3fdd822ac0ba83d
wojtko <j.w2001@onet.pl>
2023-08-07 19:19:39 +0200

    Add images.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,26 @@ import React from "react";
 import styled from "styled-components";
 
 function Header() {
-  return <Container>Header</Container>;
+  return (
+    <Nav>
+      <Logo src=""></Logo>
+      <NavMenu>
+
+      </NavMenu>
+    </Nav>
+  );
 }
 
 export default Header;
 
-const Container = styled.div`
+const Nav = styled.nav`
   height: 70px;
   background-color: #090b13;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
 `;
