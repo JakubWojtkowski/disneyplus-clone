e86737872efa539665576e1f86c3183b9b0056ce
wojtko <j.w2001@onet.pl>
2023-08-09 00:27:45 +0200

    Finish image slider.

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -20,7 +20,7 @@ function ImgSlider() {
         <img src="/images/slider-badging.jpg" alt="" />
       </Wrap>
       <Wrap>
-        <img src="/images/slider-badging.jpg" alt="" />
+        <img src="/images/slider-badag.jpg" alt="" />
       </Wrap>
     </Carousel>
   );
@@ -28,6 +28,41 @@ function ImgSlider() {
 
 export default ImgSlider;
 
-const Carousel = styled(Slider)``;
+const Carousel = styled(Slider)`
+  margin-top: 20px;
 
-const Wrap = styled.div``;
+  button {
+    z-index: 1;
+  }
+
+  ul li button {
+    &:before {
+      font-size: 10px;
+      color: rgb(150, 158, 171);
+    }
+  }
+
+  li.slick-active button:before {
+    color: #ff;
+  }
+
+  .slick-list {
+    overflow: visible;
+  }
+`;
+
+const Wrap = styled.div`
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+    box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+    border: 4px solid transparent;
+    transition: all 0.3s;
+
+    &:hover {
+      border: 4px solid rgba(249, 249, 249, 0.8);
+    }
+  }
+`;
