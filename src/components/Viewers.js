a133d9332022cb926f03ba1f7c44afbb08f2b5b1
wojtko <j.w2001@onet.pl>
2023-08-10 14:05:00 +0200

    Improve responsiveness.

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -31,6 +31,10 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(5, minmax(0, 1fr));
   grid-gap: 25px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const Wrap = styled.div`
